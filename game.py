1f2925b9b93d6ffd5652c7730f6b1373cc237839
6655321p <lucypmallen@gmail.com>
2024-04-08 11:58:52 +0100

    break the gameplay loop into seperate sections, introduce semi-event driven system

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ BG = pygame.image.load("green_bg.jpg")
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-SNAKE_SPEED = 20
+GAME_SPEED = 20
 
 
 class Game:
@@ -55,19 +55,14 @@ class Game:
 
         self.is_game_paused = False
 
-        self.gameplay_loop()
+        self.main_loop()
 
     # Pause the game
     def pause_game(self):
         self.is_game_paused = True
-        while self.is_game_paused:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.is_game_paused = False
+
+    def display_options_loop(self):
+        return
 
     # Game Over
     def on_game_over(self):
@@ -83,8 +78,101 @@ class Game:
         pygame.quit()
         quit()
 
-    # Main game loop
+    def on_key_event(self, event):
+
+        if event.key == pygame.K_ESCAPE:
+            self.pause_game()
+
+        if event.key == pygame.K_w:
+            self.change_snake_direction_to = "UP"
+        elif event.key == pygame.K_s:
+            self.change_snake_direction_to = "DOWN"
+        elif event.key == pygame.K_a:
+            self.change_snake_direction_to = "LEFT"
+        elif event.key == pygame.K_d:
+            self.change_snake_direction_to = "RIGHT"
+        elif event.key == pygame.K_SPACE:
+            self.is_game_paused = False
+
     def gameplay_loop(self):
+        if (
+            self.change_snake_direction_to == "UP"
+            and self.snake_direction != "DOWN"
+        ):
+            self.snake_direction = "UP"
+        if (
+            self.change_snake_direction_to == "DOWN"
+            and self.snake_direction != "UP"
+        ):
+            self.snake_direction = "DOWN"
+        if (
+            self.change_snake_direction_to == "LEFT"
+            and self.snake_direction != "RIGHT"
+        ):
+            self.snake_direction = "LEFT"
+        if (
+            self.change_snake_direction_to == "RIGHT"
+            and self.snake_direction != "LEFT"
+        ):
+            self.snake_direction = "RIGHT"
+
+        if self.snake_direction == "UP":
+            self.snake_position[1] -= 10
+        if self.snake_direction == "DOWN":
+            self.snake_position[1] += 10
+        if self.snake_direction == "LEFT":
+            self.snake_position[0] -= 10
+        if self.snake_direction == "RIGHT":
+            self.snake_position[0] += 10
+
+        self.snake_body.insert(0, list(self.snake_position))
+        if (
+            self.snake_position[0] == self.fruit_position[0]
+            and self.snake_position[1] == self.fruit_position[1]
+        ):
+            self.game_score += 10
+            self.fruit_position = [
+                random.randrange(1, (SCREEN_WIDTH // 10)) * 10,
+                random.randrange(1, (SCREEN_HEIGHT // 10)) * 10,
+            ]
+        else:
+            self.snake_body.pop()
+
+        self.screen.fill(YELLOW)
+
+        for pos in self.snake_body:
+            pygame.draw.rect(
+                self.screen, GREEN, pygame.Rect(pos[0], pos[1], 10, 10)
+            )
+        pygame.draw.rect(
+            self.screen,
+            RED,
+            pygame.Rect(self.fruit_position[0], self.fruit_position[1], 10, 10),
+        )
+
+        if self.snake_position[0] < 0 or self.snake_position[0] > SCREEN_WIDTH - 10:
+            self.on_game_over()
+        if (
+            self.snake_position[1] < 0
+            or self.snake_position[1] > SCREEN_HEIGHT - 10
+        ):
+            self.on_game_over()
+        for block in self.snake_body[1:]:
+            if (
+                self.snake_position[0] == block[0]
+                and self.snake_position[1] == block[1]
+            ):
+                self.on_game_over()
+
+        # Display score
+        font = pygame.font.SysFont("copperplate gothic", 24)
+        score_surface = font.render("Score: " + str(self.game_score), True, PURPLE)
+        score_rect = score_surface.get_rect()
+        score_rect.topleft = (10, 10)
+        self.screen.blit(score_surface, score_rect)
+
+    # Main game loop
+    def main_loop(self):
 
         while True:
             for event in pygame.event.get():
@@ -92,93 +180,13 @@ class Game:
                     pygame.quit()
                     quit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.pause_game()
-                    if event.key == pygame.K_w:
-                        self.change_snake_direction_to = "UP"
-                    elif event.key == pygame.K_s:
-                        self.change_snake_direction_to = "DOWN"
-                    elif event.key == pygame.K_a:
-                        self.change_snake_direction_to = "LEFT"
-                    elif event.key == pygame.K_d:
-                        self.change_snake_direction_to = "RIGHT"
+                    self.on_key_event(event)
 
-            if (
-                self.change_snake_direction_to == "UP"
-                and self.snake_direction != "DOWN"
-            ):
-                self.snake_direction = "UP"
-            if (
-                self.change_snake_direction_to == "DOWN"
-                and self.snake_direction != "UP"
-            ):
-                self.snake_direction = "DOWN"
-            if (
-                self.change_snake_direction_to == "LEFT"
-                and self.snake_direction != "RIGHT"
-            ):
-                self.snake_direction = "LEFT"
-            if (
-                self.change_snake_direction_to == "RIGHT"
-                and self.snake_direction != "LEFT"
-            ):
-                self.snake_direction = "RIGHT"
+            if self.is_game_paused:
+                self.display_options_loop()
 
-            if self.snake_direction == "UP":
-                self.snake_position[1] -= 10
-            if self.snake_direction == "DOWN":
-                self.snake_position[1] += 10
-            if self.snake_direction == "LEFT":
-                self.snake_position[0] -= 10
-            if self.snake_direction == "RIGHT":
-                self.snake_position[0] += 10
-
-            self.snake_body.insert(0, list(self.snake_position))
-            if (
-                self.snake_position[0] == self.fruit_position[0]
-                and self.snake_position[1] == self.fruit_position[1]
-            ):
-                self.game_score += 10
-                self.fruit_position = [
-                    random.randrange(1, (SCREEN_WIDTH // 10)) * 10,
-                    random.randrange(1, (SCREEN_HEIGHT // 10)) * 10,
-                ]
             else:
-                self.snake_body.pop()
+                self.gameplay_loop()
 
-            self.screen.fill(YELLOW)
-
-            for pos in self.snake_body:
-                pygame.draw.rect(
-                    self.screen, GREEN, pygame.Rect(pos[0], pos[1], 10, 10)
-                )
-            pygame.draw.rect(
-                self.screen,
-                RED,
-                pygame.Rect(self.fruit_position[0], self.fruit_position[1], 10, 10),
-            )
-
-            if self.snake_position[0] < 0 or self.snake_position[0] > SCREEN_WIDTH - 10:
-                self.on_game_over()
-            if (
-                self.snake_position[1] < 0
-                or self.snake_position[1] > SCREEN_HEIGHT - 10
-            ):
-                self.on_game_over()
-            for block in self.snake_body[1:]:
-                if (
-                    self.snake_position[0] == block[0]
-                    and self.snake_position[1] == block[1]
-                ):
-                    self.on_game_over()
-
-            # Display score
-            font = pygame.font.SysFont("copperplate gothic", 24)
-            score_surface = font.render("Score: " + str(self.game_score), True, PURPLE)
-            score_rect = score_surface.get_rect()
-            score_rect.topleft = (10, 10)
-            self.screen.blit(score_surface, score_rect)
-
-            if not self.is_game_paused:
-                pygame.display.update()
-                self.fps.tick(SNAKE_SPEED)
+            pygame.display.update()
+            self.fps.tick(GAME_SPEED)
